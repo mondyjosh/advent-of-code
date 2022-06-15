9ab3aaa0649f65d0cf8afe8ca7ebf68d56726f0d
Josh Mondy <mondyjosh@gmail.com>
2022-06-14 23:48:03 -0500

    Added tests for part 2.

--- src/csharp/AdventOfCode.Tests/Year2015/Day02Tests.cs
+++ src/csharp/AdventOfCode.Tests/Year2015/Day02Tests.cs
@@ -12,8 +12,17 @@ public class Day02SolutionTests
     [Theory]
     [MemberData(nameof(GetWrappingPaperDimensions))]
     public void SolvePart1_WithMemberData_OutputExpectedTotalArea(string input, int expected)
-    {        
+    {
         var actual = _solution.SolvePart1(input);
+
+        Assert.Equal(expected, actual);
+    }
+
+        [Theory]
+    [MemberData(nameof(GetRibbonLengths))]
+    public void SolvePart1_WithMemberData_OutputExpectedTotalLength(string input, int expected)
+    {
+        var actual = _solution.SolvePart2(input);
 
         Assert.Equal(expected, actual);
     }
@@ -24,6 +33,14 @@ public class Day02SolutionTests
         yield return new object[] { "1x1x10", 43 };
         yield return new object[] { "2x3x4\n2x3x4", 116 };
         yield return new object[] { "1x1x10\n1x1x10", 86 };
+    }
+
+    public static IEnumerable<object[]> GetRibbonLengths()
+    {
+        yield return new object[] { "2x3x4", 34 };
+        yield return new object[] { "1x1x10", 14 };
+        yield return new object[] { "2x3x4\n2x3x4", 68 };
+        yield return new object[] { "1x1x10\n1x1x10", 28 };
     }
 
     private readonly SolutionBase _solution;
